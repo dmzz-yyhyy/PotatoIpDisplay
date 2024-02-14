2695e9d288f5b71fbff47e187b1d91a903dafada
dmzz-yyhyy <hk198580666@outlook.com>
2024-02-15 03:29:28 +0800

    1.1 -修复国外ip无法显示问题

--- src/main/kotlin/indi/nightfish/potato_ip_display/PlayerJoinListener.kt
+++ src/main/kotlin/indi/nightfish/potato_ip_display/PlayerJoinListener.kt
@@ -9,7 +9,7 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerLoginEvent
 
 
-class PlayerJoinListener: Listener{
+class PlayerJoinListener : Listener {
 
     @EventHandler
     fun onPlayerLoin(event: PlayerLoginEvent) {
@@ -18,11 +18,17 @@ class PlayerJoinListener: Listener{
         val playerName = player.name
         val resJson = HttpClient5Get.get("https://whois.pconline.com.cn/ipJson.jsp?ip=$playerAddress&json=true")
         val gson = Gson().fromJson(resJson, IpData::class.java)
-        val pro = gson.pro.replace("省","")
         val addr = gson.addr
+        val pro = if (gson.pro.replace("省", "") == "") {
+            Regex(pattern = """^[\u4e00-\u9fa5]+""")
+                .find(addr)?.value ?: ""
+        } else {
+            gson.pro.replace("省", "")
+        }
         IpATTRMap.playerIpATTRMap[playerName] = pro
         Bukkit.getServer().logger.info("Player named $playerName connect to proxy from $addr")
     }
+
     @EventHandler
     fun onPlayerLogin(event: PlayerJoinEvent) {
         event.player.sendMessage("${ChatColor.DARK_GRAY}[${ChatColor.GOLD}PotatoIpDisplay${ChatColor.DARK_GRAY}] ${ChatColor.YELLOW}您当前ip归属地 ${ChatColor.GRAY}[${ChatColor.AQUA}${IpATTRMap.playerIpATTRMap[event.player.name]}${ChatColor.GRAY}]${ChatColor.RESET}")
