da24c1190ece623fe93eda126ebc430c7bc24942
yukonisen <whenten99@gmail.com>
2026-08-19 05:16:54 +0800

    更新当前项目结构

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,9 @@
-import java.util.Properties
-
-private lateinit var metadata: Properties
-
 plugins {
     idea
 }
 
-fun Project.requireMetadata(): Properties {
-    if (!::metadata.isInitialized) {
-        metadata = Properties().apply {
-            load(rootProject.file("PLUGIN_VERSION").inputStream())
-        }
-    }
-    return metadata
-}
-
-fun getMetadata(key: Any): Any {
-    return when (key) {
-        "version" -> requireMetadata().getProperty("PLUGIN")
-        else -> "?"
-    }
-}
-
 allprojects {
     group = "indi.nightfish.potato_ip_display"
-    version = getMetadata("version")
+    version = "1.5.0"
     description = "Display the IP attribution of players."
 }
